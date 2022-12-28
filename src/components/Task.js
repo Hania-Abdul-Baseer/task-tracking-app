9bf4385b211fa6ae5709ec87d7fbbe2bad285ea9
Hania-Abdul-Baseer <76831541+Hania-Abdul-Baseer@users.noreply.github.com>
2022-12-28 14:02:18 +1030

    s

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,9 +1,14 @@
+// Using these react icons allows us to use 
+// icons as components.
+import { FaTimes } from 'react-icons/fa'
+
 const Task = ({ task }) => {
     
     return (
       <div className='task'>
           <h3>
-              {task.text} 
+              {task.text} <FaTimes style={{ color: 
+                'red', cursor: 'pointer' }} />
           </h3>
           <p>{task.day}</p>
       </div>
--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,8 +1,14 @@
+import Task from './Task'
+
 const Tasks = ({ tasks }) => {
   return (
     <>
+        {/* We are looping through each task in the
+        tasks array, and outputing a Task component
+        for each while also passing the task itself as
+        a prop to the Task component. */}
         {tasks.map((task) => (
-            <h3 key={task.id}>{task.text}</h3>
+            <Task key={task.id} task={task}/>
         ))}
     </>
   )
